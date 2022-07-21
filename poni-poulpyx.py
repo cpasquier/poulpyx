0301ec7df6393703c0a674b33c70c8f18256b6fc
Coralie Pasquier <coralie.pasquier1@gmail.com>
2022-07-21 14:18:43 +0200

    Update poni-poulpyx.py
    Update 21-07-22

--- poni-poulpyx.py
+++ poni-poulpyx.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import os
+import tkinter as tk
 import tkinter.filedialog as fd
-from tkinter import Tk
+from tkinter import Tk,DoubleVar,Entry,Button
 plt.rcParams.update({'font.size': 16, 'figure.figsize': [12.0, 6.0]})
 
-ponidir = '.' #'/home/mar345/data/LineUp'
+ponidir = '.'
 if os.path.isdir(ponidir):
     root = Tk()
     ponifile = fd.askopenfile(parent=root, initialdir=ponidir, title='Select a poni file')
@@ -16,10 +17,23 @@ for line in ponilines:
         a = line.split()
         distance = a[1]
 
-gu = Tk()
-rptlist = fd.askopenfilenames(parent=gu, title='Select rpt files', filetypes=[("rpt files", ".rpt")])
-gu.destroy()
+def close_window():       #function to close the window
+    gu1.destroy()
+
+gu1 = Tk()      #to get the K value previously determinated
+tk.Label(gu1, text="K =").grid(row=0, column=0)
+d1 = DoubleVar(gu1)  
+e1 = Entry(gu1, textvariable=d1).grid(row=0, column=1)
+button1 = tk.Button(text = "OK", command = close_window, width=6, height=2).grid(row=1,column=1, pady=15)
+gu1.mainloop()
+
+kset = d1.get()
+
+gu2 = Tk()     #modify rpt file (add K,d)
+rptlist = fd.askopenfilenames(parent=gu2, title='Select rpt files', filetypes=[("rpt files", ".rpt")])
+gu2.destroy()
 for file in rptlist:
     with open(file, 'a') as rpt:
         rpt.write('d = '+str(distance)+'\n')
+        rpt.write('k = '+str(kset)+'\n')  
         rpt.close()
